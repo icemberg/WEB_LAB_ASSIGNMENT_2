c3fc11e8f70258d51288315ac0a8d5ecee6e115f
icemberg <135527820+icemberg@users.noreply.github.com>
2024-12-21 09:00:27 +0530

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // Database connection settings
 $servername = "sql12.freemysqlhosting.net";
 $username = "sql12753342";
